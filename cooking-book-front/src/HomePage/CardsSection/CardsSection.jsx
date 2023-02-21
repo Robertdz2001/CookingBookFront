5f14f824a4b8db4a9170dad5c7f6ec45e7f18530
Robertdz2001 <robertdz0@onet.pl>
2023-02-21 19:48:18 +0100

    Add dynamic cards info

--- cooking-book-front/src/HomePage/CardsSection/CardsSection.jsx
+++ cooking-book-front/src/HomePage/CardsSection/CardsSection.jsx
@@ -1,8 +1,36 @@
 import classes from "./CardsSection.module.css";
-
+import { useEffect, useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 function CardsSection() {
+  const [topRecipes, setTopRecipes] = useState(false);
+
+  const getTopRecipes = async () => {
+    try {
+      const response = await axios.get(
+        "https://localhost:44371/api/recipes/top"
+      );
+
+      return response.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    async function getData() {
+      const response = await getTopRecipes();
+      setTopRecipes(response);
+      // console.log(topRecipes);
+    }
+    getData();
+  }, []);
+
+  if (!topRecipes) {
+    return <div>LOADING...</div>;
+  }
+
   return (
     <section
       id="cards-id"
@@ -10,33 +38,21 @@ function CardsSection() {
       <span className={classes["cards-subheading"]}>Recipes</span>
       <h2 className={classes["cards-heading"]}>Explore new tastes</h2>
       <div className={classes["cards"]}>
-        <Card
-          recipeImg="images/pizza.jpg"
-          title="Neopolitan Pizza"
-          user="Cathrina"
-          userImg="images/customer-1.jpg"
-          calories="1200"
-          rating="4.6"
-          time="80"
-        />
-        <Card
-          recipeImg="images/burger.jpg"
-          title="Burger with fries"
-          user="Viktor"
-          userImg="images/customer-2.jpg"
-          calories="1400"
-          rating="4.8"
-          time="50"
-        />
-        <Card
-          recipeImg="images/toast.jpg"
-          title="Toasts with eggs"
-          user="Joetta"
-          userImg="images/customer-3.jpg"
-          calories="300"
-          rating="4.4"
-          time="15"
-        />
+        {topRecipes.map((recipe) => {
+          return (
+            <Card
+              key={"card" + recipe.id}
+              recipeImg={recipe.imageUrl}
+              title={recipe.name}
+              user={recipe.user.userName}
+              userImg={recipe.user.imageUrl}
+              calories={recipe.calories}
+              rating={0.5 * (recipe.recipeRating / recipe.reviews.length) + 2.5}
+              time={recipe.prepTime}
+              reviewsCount={recipe.reviews.length}
+            />
+          );
+        })}
       </div>
     </section>
   );
