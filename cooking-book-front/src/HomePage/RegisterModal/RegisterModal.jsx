50b15afa9e88d0b63adbd05b2542e5dc51a441d1
Robertdz2001 <robertdz0@onet.pl>
2023-02-23 16:30:39 +0100

    Style register modal. Hide sticky navbar when modal is open.

--- cooking-book-front/src/HomePage/RegisterModal/RegisterModal.jsx
+++ cooking-book-front/src/HomePage/RegisterModal/RegisterModal.jsx
@@ -6,11 +6,30 @@ function RegisterModal(props) {
       <dialog open className={classes.modal}>
         <span className={classes["heading"]}>Register</span>
         <form className={classes["register-form"]}>
-          <input type="text" className={classes["register-input"]} />
-          <input type="text" className={classes["register-input"]} />
-          <input type="text" className={classes["register-input"]} />
-          <input type="text" className={classes["register-input"]} />
-          <button>Create Account</button>
+          <input
+            type="text"
+            className={classes["register-input"]}
+            placeholder="User Name"
+          />
+          <input
+            type="text"
+            className={classes["register-input"]}
+            placeholder="Image Url"
+          />
+          <input
+            type="password"
+            className={classes["register-input"]}
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            className={classes["register-input"]}
+            placeholder="Confirm Password"
+          />
+          <button
+            className={`${classes["btn"]} ${classes["btn--main"]} ${classes["btn--form"]}`}>
+            Create Account
+          </button>
         </form>
       </dialog>
     </div>
